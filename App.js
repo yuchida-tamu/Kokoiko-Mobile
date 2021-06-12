21b890e968172147a9aedd9ee30eda9a6bf36781
yuchida-tamu <yutaside1992@gmail.com>
2021-06-12 11:45:11 +0900

    setup venues-screen

--- App.js
+++ App.js
@@ -1,21 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import { VenuesScreen } from "./src/features/venues/screens/venues.screen";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <VenuesScreen />;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create({});
---
+++ src/features/venues/screens/venues.screen.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { SafeAreaView, Text } from "react-native";
+
+export const VenuesScreen = () => {
+  return (
+    <SafeAreaView>
+      <Text>Venues Screen</Text>
+    </SafeAreaView>
+  );
+};
