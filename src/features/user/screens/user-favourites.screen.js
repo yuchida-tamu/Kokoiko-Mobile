7556f09b2aa77923d2de83bc4884ba3d57497781
yuchida-tamu <yutaside1992@gmail.com>
2021-11-02 15:20:20 +0900

    feat: add tentative user favourite screen

---
+++ src/features/user/screens/user-favourites.screen.js
@@ -0,0 +1,9 @@
+import { SafeArea } from '../../../components/utilities/safe-area.component';
+
+export const UserFavouritesScreen = ({ route }) => {
+  return (
+    <SafeArea>
+      <Text>UserFavourites</Text>
+    </SafeArea>
+  );
+};
