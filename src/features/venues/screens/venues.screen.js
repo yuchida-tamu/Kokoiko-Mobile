118884d3a42366ee01ee290d8d4682525048e056
yuchida-tamu <yutaside1992@gmail.com>
2021-11-02 11:50:12 +0900

    refactor: memoization of renderItem and keyExtractor

--- src/features/venues/screens/venues.screen.js
+++ src/features/venues/screens/venues.screen.js
@@ -1,20 +1,20 @@
-import React, { useContext, useState } from "react";
-import { TouchableOpacity } from "react-native";
-import { ActivityIndicator } from "react-native-paper";
+import React, { useCallback, useContext, useState } from 'react';
+import { TouchableOpacity } from 'react-native';
+import { ActivityIndicator } from 'react-native-paper';
 //context
-import { VenuesContext } from "../../../services/venues/venues.context";
+import { VenuesContext } from '../../../services/venues/venues.context';
 //component
-import { VenueInfoCard } from "../components/venue-info-card.component";
+import { VenueInfoCard } from '../components/venue-info-card.component';
 import {
   ScreenBackground,
   VenueList,
   ListContainer,
-} from "../components/venues-styles.component";
-import { SafeArea } from "../../../components/utilities/safe-area.component";
-import { Spacer } from "../../../components/spacer/spacer.component";
-import { SearchBarComponent } from "../../../components/searchbar/searchbar.component";
+} from '../components/venues-styles.component';
+import { SafeArea } from '../../../components/utilities/safe-area.component';
+import { Spacer } from '../../../components/spacer/spacer.component';
+import { SearchBarComponent } from '../../../components/searchbar/searchbar.component';
 //infra
-import { colors } from "../../../infrastructure/theme/colors";
+import { colors } from '../../../infrastructure/theme/colors';
 
 export const VenuesScreen = ({ navigation }) => {
   const {
@@ -22,6 +22,20 @@ export const VenuesScreen = ({ navigation }) => {
     isLoading: isLoadingVenues,
     error,
   } = useContext(VenuesContext);
+
+  const renderItem = useCallback(({ item }) => {
+    <Spacer position="bottom" size="large">
+      <TouchableOpacity
+        onPress={() => {
+          navigation.navigate('VenueDetail', { item: item.item });
+        }}
+      >
+        <VenueInfoCard venue={item.item} />
+      </TouchableOpacity>
+    </Spacer>;
+  }, []);
+
+  const keyExtractor = useCallback((item) => item.name + 'id', []);
 
   return (
     <SafeArea>
@@ -37,18 +51,8 @@ export const VenuesScreen = ({ navigation }) => {
           ) : (
             <VenueList
               data={venues}
-              renderItem={(venue) => (
-                <Spacer position="bottom" size="large">
-                  <TouchableOpacity
-                    onPress={() => {
-                      navigation.navigate("VenueDetail", { item: venue.item });
-                    }}
-                  >
-                    <VenueInfoCard venue={venue.item} />
-                  </TouchableOpacity>
-                </Spacer>
-              )}
-              keyExtractor={(item) => item.name + "id"}
+              renderItem={renderItem}
+              keyExtractor={keyExtractor}
             />
           )}
         </ListContainer>
