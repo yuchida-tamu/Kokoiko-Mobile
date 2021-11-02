2f323a7b819f07dfc1a2b9746a3263720157b991
yuchida-tamu <yutaside1992@gmail.com>
2021-11-02 11:55:37 +0900

    refactor: move out styled components to other file

--- src/features/venues/components/venue-detail-styles.component.js
+++ src/features/venues/components/venue-detail-styles.component.js
@@ -1,4 +1,46 @@
-import { View, Text, ScrollView } from "react-native";
-import styled from "styled-components/native";
-import { Card } from "react-native-paper";
-import { Ionicons } from "@expo/vector-icons";
+import { View, Text, ScrollView } from 'react-native';
+import styled from 'styled-components/native';
+import { Card } from 'react-native-paper';
+import { Ionicons } from '@expo/vector-icons';
+
+export const AccordionList = styled(List.Accordion).attrs({
+  titleStyle: {
+    color: colors.text.primary,
+    fontFamily: fonts.heading,
+  },
+})`
+  background-color: ${(props) => props.theme.colors.bg.secondary};
+  color: ${(props) => props.theme.colors.text.primary};
+  width: 100%;
+  padding: ${(props) => props.theme.space[2]} ${(props) => props.theme.space[0]};
+`;
+
+export const DescriptionContainer = styled(View)`
+  background-color: ${(props) => props.theme.colors.bg.secondary};
+  height: ${(props) => props.theme.sizes[4]};
+  width: ${(props) => props.theme.sizes[6]};
+  padding-right: ${(props) => props.theme.space[4]};
+  align-items: center;
+`;
+
+export const Description = styled(Text)`
+  color: ${(props) => props.theme.colors.text.muted};
+`;
+
+export const ListItem = styled(List.Item).attrs({
+  titleStyle: {
+    color: colors.text.primary,
+    fontFamily: fonts.body,
+  },
+  descriptionStyle: {
+    color: colors.text.muted,
+    fontSize: fontSizes.caption,
+  },
+})`
+  background-color: ${(props) => props.theme.colors.bg.secondary};
+  color: ${(props) => props.theme.colors.text.primary};
+`;
+
+export const ListIcon = styled(List.Icon).attrs({
+  color: colors.ui.primary,
+})``;
